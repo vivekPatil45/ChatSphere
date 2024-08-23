dfd54be3b1a79efd7fbb341ca00c05be762e837c
Vivek Patil <vivekrp4503@gmail.com>
2024-08-23 11:48:57 +0530

    fix bug

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -83,7 +83,7 @@ export const login = async (req, res, next) => {
         }
 
         res.cookie("jwt", createToken(email, user._id), {
-x            maxAge,
+            maxAge,
             httpOnly: true,
             secure: true,
             sameSite: "None",
